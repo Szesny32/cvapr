a1488e35958f1dd4e295da4c596dfce33a8dc394
Kacklos <61699095+Kacklos@users.noreply.github.com>
2023-05-21 13:55:58 +0200

    Update main.py
    balanced dziala

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from pathlib import Path
 from typing import List, Tuple, Dict
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_validate
-from sklearn.metrics import log_loss, confusion_matrix
+from sklearn.metrics import log_loss, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 import addcopyfighandler # enables ctrl + c -> save matplotlib figure to clipboard
 
-
-# plt.rcParams.update({'font.size': 22})
+plt.rcParams.update({'font.size': 20})
 #plt.style.use('seaborn')
 
 class KickstartedPredict():
@@ -94,7 +93,8 @@ class KickstartedPredict():
 
         # Convert categorical data
         self.df_prepared: pd.DataFrame = pd.get_dummies(self.df, drop_first=True)
-
+        print(self.df.loc[1])
+        print(self.df_prepared.loc[1])
         # Convert NaN to 0
         #print(self.df_prepared.isna().sum())  # two blurb_word_len is NaN
         self.df_prepared.blurb_word_len.fillna(0, inplace=True)
@@ -117,7 +117,6 @@ class KickstartedPredict():
 
         for col in X_all.columns[:]:
             self.logistic_regression_SISO(np.array(X_all[col]).reshape(-1, 1), np.array(y), col_label = col)
-
 
     def logistic_regression_SISO(self, X: np.ndarray, y: np.ndarray, use_scaler: bool = True, col_label: str = "", plot_conf_matrix: bool = False) -> None:
         def sensitivity(clf, X, y) -> float:
@@ -172,25 +171,28 @@ class KickstartedPredict():
             bic = -2 * log_likelihood+ num_params * np.log(n)
             return bic
 
-
         # X_test, y_test -> hold-out set
         X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.6, random_state=123)
 
         scaler = StandardScaler()
-        logreg = LogisticRegression(C=10, fit_intercept=True, penalty='l2')
+        logreg = LogisticRegression(C=10, fit_intercept=True, penalty='l2', class_weight = "balanced")
         if use_scaler:
             pipeline:Pipeline = Pipeline([('scaler', scaler), ('logreg', logreg )])
         else:
             pipeline:Pipeline = Pipeline([('logreg', logreg)])
-
 
         # use optional scaler and fit train data
         clf = pipeline.fit(X_train, y_train)
 
         #Plot conf matrix
         if plot_conf_matrix:
-            plot_confusion_matrix(clf, X, y)
-            plt.title("SISO: " + col_label)
+            ConfusionMatrixDisplay.from_estimator(
+                clf,
+                X_test,
+                y_test,
+                normalize="true"
+            )
+            plt.title("SISO: " + col_label + " Balanced")
             plt.show()
 
         self.scores_for_cross_validation = {
@@ -202,7 +204,8 @@ class KickstartedPredict():
             "balanced_accuracy": "balanced_accuracy",
             'f1': 'f1',
         }
-        scores = cross_validate(clf, X, y, cv=5, scoring = self.scores_for_cross_validation, return_train_score = False)
+        scores = cross_validate(clf, X, y, cv=15, scoring = self.scores_for_cross_validation, return_train_score = False, return_estimator=False)
+
 
         # Calculate mean from k-validations and add to score_df
         col_score = {}
@@ -233,7 +236,7 @@ class KickstartedPredict():
             if param == "test_BIC":
                 asc_bool = True
 
-            plt.figure(figsize=(16, 9))
+            plt.figure(figsize=(16, 11))
             self.score_df[param].sort_values(ascending=asc_bool).plot(kind="bar")
             bar_x = self.score_df[param].sort_values(ascending=asc_bool).index
             bar_y = self.score_df[param].loc[bar_x]
@@ -245,104 +248,9 @@ class KickstartedPredict():
 
             plt.errorbar(bar_x, bar_y, yerr=bar_err, fmt = 'o',color = 'black',
             ecolor = 'black', elinewidth = 2, capsize=10, capthick = 2)
-            plt.title(param)
+            plt.title(param+" Balanced")
             plt.tight_layout()
             plt.show()
-
-
-#
-# def main():
-#     pd.options.display.max_columns = 9999
-#     pd.options.display.max_rows = 10
-#     # df = pd.read_csv(r"C:\Users\kbklo\Desktop\Studia\_INFS2\CVaPR\Projekt\Data\Kickstarter010.csv",
-#     #                  )
-#     # # print(read_df.columns)
-#     # # df = read_df[columns_to_read]
-#     # print(df.columns)
-#
-#     init_filename = r"C:\Users\kbklo\Desktop\Studia\_INFS2\CVaPR\Projekt\Data\Kickstarter.csv"
-#     # used_columns = ["goal", "state", "usd_pledged", "launched_at", "spotlight","deadline"]
-#     used_columns = ["state","launched_at", "deadline"]
-#
-#     # Init dataframe for merge
-#     df = pd.read_csv(init_filename, usecols=used_columns,)
-#
-#
-#     n_of_files = 100
-#     for i, filename in enumerate(os.scandir(r"C:\Users\kbklo\Desktop\Studia\_INFS2\CVaPR\Projekt\Data")):
-#         if filename.is_file():
-#
-#             if filename != init_filename:
-#                 new_df = pd.read_csv(filename, usecols=used_columns,)
-#                 df = pd.concat([df, new_df])
-#
-#                 i +=1
-#                 if i > n_of_files:
-#                     break
-#
-#     fig, ax = plt.subplots(1,1)
-#
-#
-#     df['launched_at'] = pd.to_datetime(df['launched_at'], unit ='s')
-#     df['deadline'] = pd.to_datetime(df['deadline'], unit ='s')
-#
-#     df['duration'] = (df['deadline'] - df['launched_at']).dt.days
-#
-#     print(df)
-#     print(df.groupby('duration'))
-#     success_rate = df.groupby('duration').state.count().plot(kind = 'bar', rot = 0)
-#     print(df['duration'].max())
-#
-#     xtick = np.char.mod('%d', range(0 , 105))
-#     xtick[:] = ""
-#     xtick[::20] = np.char.mod('%d', range(0 ,105, 20))
-#
-#     plt.xticks(range(0 , 105),xtick )
-#     plt.ylabel("Amount of Projects")
-#     plt.xlabel("Campaign Duration (Days)")
-#     plt.title("Amount of Projects by Duration")
-#     plt.show()
-#     print(success_rate)
-#     # fraction_rate = success_rate/df.groupby('duration').count()
-#     # fraction_rate.plot(kind = 'bar')
-#     plt.title("Median usd_pledged value by state")
-#     plt.ylabel("Median usd_pledged value")
-#     plt.show()
-#     df = df[df['state'].isin(['successful', 'failed'])] # drop other states than ['successful', 'failed']
-#     # df.groupby('state').goal.median().plot(kind='bar', rot=0) #, color=['darkred', 'green']
-#     # annotate
-#     print(df.groupby('state').describe().astype(int))
-#
-#     print(np.array(df['goal']))
-#
-#     hist_max_goal = 1E5
-#     s_hist_data = np.array(df['goal'][df['state']=="successful"])
-#     s_hist_data = np.clip(s_hist_data, 0.0, hist_max_goal)
-#     f_hist_data = np.array(df['goal'][df['state'] == "failed"])
-#     f_hist_data = np.clip(f_hist_data, 0.0, hist_max_goal)
-#     s_w = np.empty(s_hist_data.shape)
-#     s_w.fill(1 / s_hist_data.shape[0])
-#     f_w = np.empty(f_hist_data.shape)
-#     f_w.fill(1 / f_hist_data.shape[0])
-#
-#
-#     hist_data = ax.hist([s_hist_data, f_hist_data], bins =10, weights = [s_w, f_w], range = (0,hist_max_goal), align = 'mid', color = ["green", "darkred"])
-#     bin_edges = hist_data[1]
-#     bin_edges_labels = np.copy(bin_edges)
-#     bin_edges_labels = np.char.mod('%d', bin_edges_labels)
-#     for bin_i in range(len(bin_edges_labels)):
-#         bin_edges_labels[bin_i] = bin_edges_labels[bin_i][:2] + " " + bin_edges_labels[bin_i][2:]
-#     bin_edges_labels[-1] = ""
-#     plt.xticks(bin_edges, bin_edges_labels)
-#     plt.xlabel("Goal ($)")
-#     plt.ylabel("Fraction of projects")
-#
-#     print(1E6)
-#     # plt.yscale("log")
-#     # plt.bar_label(ax.containers[0], label_type='edge')
-#     plt.legend(["successful", "failure"])
-#     plt.title("Project goal distribution")
-#     plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
